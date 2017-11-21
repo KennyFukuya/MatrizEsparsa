76f11b3f15845c5cd84d9b19c83189b3e19f1544
KennyFukuya <KennyFukuya@github.com>
2017-11-21 00:02:31 -0200

    Tentativa de manipular arquivo

--- Trabalho.c
+++ Trabalho.c
@@ -17,8 +17,9 @@ int main(int argc, char const *argv[]) {
 	int opcao, sair = 0, escolha_me, i;
 	MatrizEsparsa me1, me2;
 	int l1, c1, l2, c2, l, c, valor;
+	char endereco1[100]="";
 	do{
-		printf("\n\n\n	Escolha a opção desejada:\n	1. Carregar uma matriz do arquivo\n	2. Visualizar uma matriz\n	3. Modificar uma matriz, dadas as coordenadas (linha e coluna)\n	4. Salvar uma matriz\n	5. Soma entre matrizes\n	6. Subtração entre matrizes\n	7. Multiplicação entre matrizes\n	8. Sair do programa\n	");
+		printf("\n\n\n	Escolha a opção desejada:\n	1. Carregar uma matriz do arquivo\n	2. Visualizar uma matriz\n	3. Modificar uma matriz, dadas as coordenadas (linha e coluna)\n	4. Salvar uma matriz\n	5. Soma entre matrizes\n	6. Subtração entre matrizes\n	7. Multiplicação entre matrizes\n	8. Sair do programa\n\n	");
 		scanf("%i",&opcao);
 		switch(opcao){
 			case 1:
@@ -26,12 +27,19 @@ int main(int argc, char const *argv[]) {
 				scanf("%i",&escolha_me);
 				switch(escolha_me){
 					case 1:
-						printf("\n	Informe o numero de linhas: ");
-						scanf("%i",&l1);
-						printf("\n	Informe o numero de colunas: ");
-						scanf("%i",&c1);
-						inicializa_matrizesp(&me1,l1,c1);
-						printf("\n	Matriz 1 inicializada.");
+						FILE *arq1;
+						printf("\n	Digite o endereço do arquivo txt da matriz desejada: ");
+						scanf("%s",&endereco1);
+						arq1=fopen("matriz1.txt", "a");
+						if(arq1==NULL){
+							printf("\n%s\n Arquivo não encontrado!",arq1);	
+						}
+					//	printf("\n	Informe o numero de linhas: ");
+					//	scanf("%i",&l1);
+					//	printf("\n	Informe o numero de colunas: ");
+					//	scanf("%i",&c1);
+					//	inicializa_matrizesp(&me1,l1,c1);
+					//	printf("\n	Matriz 1 inicializada.");
 						break;
 					case 2:
 						printf("\n	Informe o numero de linhas: ");
@@ -48,10 +56,10 @@ int main(int argc, char const *argv[]) {
 				scanf("%i",&escolha_me);
 				switch(escolha_me){
 					case 1:
-						mostra_m(&me1);
+						mostra_m(&me1,escolha_me);
 						break;
 					case 2:
-						mostra_m(&me2);
+						mostra_m(&me2,escolha_me);
 						break;
 				}
 				break;
@@ -66,7 +74,7 @@ int main(int argc, char const *argv[]) {
 						scanf("%i",&c);
 						printf("\n	Informe o valor: ");
 						scanf("%i",&valor);
-						modifica_matriz(&me1, l, c, valor);
+						modifica_matriz(&me1, l, c, valor,escolha_me);
 						break;
 					case 2:
 						printf("\n	Informe a linha: ");
@@ -102,7 +110,7 @@ int main(int argc, char const *argv[]) {
   return 0;
 }
 
-void modifica_matriz(MatrizEsparsa *me, int l, int c, int valor){
+void modifica_matriz(MatrizEsparsa *me, int l, int c, int valor,int escolha_me){
 	system("cls"); //Limpa a tela.
 	if(checa_me_inicializou(*me)){
 		int tam_entrada = sizeof(me->m[l])/sizeof(EntradaMatriz);
@@ -113,7 +121,7 @@ void modifica_matriz(MatrizEsparsa *me, int l, int c, int valor){
 		me->m[l] = realloc(me->m[l], sizeof(EntradaMatriz)*tam_entrada);
 		me->m[l]->coluna = c;
 		me->m[l]->valor = valor;
-		mostra_m(*me);
+		mostra_m(*me,escolha_me);
 	}else{
 		printf("\n	Matriz não inicializada.");
 	}
@@ -127,23 +135,24 @@ void inicializa_matrizesp(MatrizEsparsa *me, int l, int c){
 }
 
 
-void mostra_m(MatrizEsparsa *me){
+void mostra_m(MatrizEsparsa *me,int escolha_me){
 	system("cls"); //Limpa a tela.
 	if(checa_me_inicializou(*me)){
 		int i,j;
+		printf("\n	Matriz %i modificada:\n",escolha_me); //Coloquei pra mostrar qual matriz foi modificada (Kenny).
   		for(i=0;i<(me->num_linhas);i++){
 	    	if((me->m[i])==NULL){
 		    	for(j=0;j<(me->num_colunas);j++){
-		    		printf(" 0");
+		    		printf("	0");
 				}
     		}else{
     			int k = 0;
     			for(j=0;j<(me->num_colunas);j++){
 		    		if(me->m[i][k].coluna==j){
-		    			printf(" %.2f",me->m[i][k].valor);
+		    			printf("	%.2f",me->m[i][k].valor);
 		    			k++;
 					}else{
-						printf(" 0");
+						printf("	0");
 					}
 				}
 			}
